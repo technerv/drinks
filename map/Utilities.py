8a0288d9afd0e6cb08218623aacd65cbaeefca8a
Evans Githinji <evanoskill@gmail.com>
2018-04-30 16:41:53 +0300

    Added a utility for random number generation

---
+++ map/Utilities.py
@@ -0,0 +1,8 @@
+from django.template.defaultfilters import slugify
+
+
+import random
+import string
+
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
