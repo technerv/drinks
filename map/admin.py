59e9dc790aa3f9a50dde4ea3b9ec62e37bebd9c3
Evans Githinji <evanoskill@gmail.com>
2018-04-30 16:40:57 +0300

    Configured the default admin site and created the superuser. Admin backend working successfully.

--- map/admin.py
+++ map/admin.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib.gis import admin
+from models import *
 
 # Register your models here.
+admin.site.register(Country, admin.OSMGeoAdmin)
+admin.site.register(County, admin.OSMGeoAdmin)
+admin.site.register(Scounty, admin.OSMGeoAdmin)
+admin.site.register(Ward, admin.OSMGeoAdmin)
+admin.site.register(Business, admin.OSMGeoAdmin)
+admin.site.register(CommunityAmmenity, admin.OSMGeoAdmin)
+admin.site.register(BusinessType, admin.ModelAdmin)
+admin.site.register(CommunityAmmenityType, admin.ModelAdmin)
+admin.site.register(Incident, admin.OSMGeoAdmin)
+admin.site.register(IncidentType, admin.ModelAdmin)
+admin.site.register(LandUse, admin.ModelAdmin)
+
