04ec4352e2b426fafc1e7029ecd5180767ffaeaa
Evans Githinji <evanoskill@gmail.com>
2018-05-01 15:33:16 +0300

    updated the urls.py file.created app name, set url patterns to their respective geojson data

--- map/urls.py
+++ map/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url,include
-from .views import index
-
+from .views import *
+app_name = 'map'
 urlpatterns = [
-    url(r'^', index, name='map')
+    url(r'^$', index, name='map'),
+    url(r'^ward.geojson$', ward_data, name='ward_data'),
+    url(r'^country.geojson$', country_data, name='country_data'),
+    url(r'^county.geojson$', county_data, name='county_data'),
+    url(r'^scounty.geojson$', scounty_data, name='scounty_data'),
+    url(r'^business.geojson$', business_data, name='business_data'),
+    url(r'^amenity.geojson$', amenity_data, name='amenity_data'),
+    url(r'^incident.geojson$', incident_data, name='incident_data'),
 ]
